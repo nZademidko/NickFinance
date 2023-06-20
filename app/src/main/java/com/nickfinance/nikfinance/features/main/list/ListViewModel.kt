f9ab882e5bbac674424cc7211bbc054e90413992
Nikita <fanban3377@gmail.com>
2023-06-20 10:18:18 +0500

    almost final commit

--- app/src/main/java/com/nickfinance/nikfinance/features/main/list/ListViewModel.kt
+++ app/src/main/java/com/nickfinance/nikfinance/features/main/list/ListViewModel.kt
@@ -1,8 +1,6 @@
 package com.nickfinance.nikfinance.features.main.list
 
 import android.app.Application
-import androidx.lifecycle.viewModelScope
-import com.google.android.material.datepicker.MaterialDatePicker
 import com.nickfinance.nikfinance.base.adapter.BaseRowHolder
 import com.nickfinance.nikfinance.base.BaseViewModel
 import com.nickfinance.nikfinance.base.adapter.holders.SingleLineViewHolder
@@ -10,12 +8,10 @@ import com.nickfinance.nikfinance.base.adapter.holders.TimeHolder
 import com.nickfinance.nikfinance.base.imageLoader.ColorState
 import com.nickfinance.nikfinance.base.views.builders.*
 import com.nickfinance.nikfinance.domain.models.ExpenseWithTheme
-import com.nickfinance.nikfinance.domain.repositories.abstractions.MainRepository
 import com.nickfinance.nikfinance.features.main.pager.MainPagerFragmentDirections
 import com.nickfinance.nikfinance.utils.DateFormats
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
 
@@ -31,12 +27,22 @@ class ListViewModel @Inject constructor(
         var curTime: Long? = null
         val list = mutableListOf<BaseRowHolder>()
         data.forEach { item ->
-            if (curTime == null || curTime != item.expense.createdAt - (item.expense.createdAt % 86_400_000)) {
-                curTime = item.expense.createdAt - (item.expense.createdAt % 86_400_000)
-                val timeInMIlls = Calendar.getInstance().timeInMillis
-                val result = if (timeInMIlls - (timeInMIlls % 86_400_000) == curTime!!) {
+            val createdAtCalendar = Calendar.getInstance()
+            createdAtCalendar.timeInMillis = item.expense.createdAt
+            createdAtCalendar.set(Calendar.HOUR_OF_DAY, 0)
+            createdAtCalendar.set(Calendar.MINUTE, 0)
+            createdAtCalendar.set(Calendar.SECOND, 0)
+            createdAtCalendar.set(Calendar.MILLISECOND, 0)
+            if (curTime == null || curTime != createdAtCalendar.timeInMillis) {
+                curTime = createdAtCalendar.timeInMillis
+                val todayCalendar = Calendar.getInstance()
+                todayCalendar.set(Calendar.HOUR_OF_DAY, 0)
+                todayCalendar.set(Calendar.MINUTE, 0)
+                todayCalendar.set(Calendar.SECOND, 0)
+                todayCalendar.set(Calendar.MILLISECOND, 0)
+                val result = if (todayCalendar.timeInMillis == curTime!!) {
                     "Сегодня"
-                } else if (timeInMIlls - (timeInMIlls % 86_400_000) - curTime!! <= 86_400_000) {
+                } else if (todayCalendar.timeInMillis - curTime!! <= 86_400_000) {
                     "Вчера"
                 } else {
                     DateFormats.getString(
@@ -75,6 +81,51 @@ class ListViewModel @Inject constructor(
                     .build()
             ))
         }
+//        data.forEach { item ->
+//            if (curTime == null || curTime != item.expense.createdAt - (item.expense.createdAt % 86_400_000)) {
+//                curTime = item.expense.createdAt - (item.expense.createdAt % 86_400_000)
+//                val timeInMIlls = Calendar.getInstance().timeInMillis
+//                val result = if (timeInMIlls - (timeInMIlls % 86_400_000) == curTime!!) {
+//                    "Сегодня"
+//                } else if (timeInMIlls - (timeInMIlls % 86_400_000) - curTime!! <= 86_400_000) {
+//                    "Вчера"
+//                } else {
+//                    DateFormats.getString(
+//                        Date(item.expense.createdAt),
+//                        DateFormats.Companion.Format.DD_MMMM
+//                    )
+//                }
+//                list.add(TimeHolder(time = result))
+//            }
+//            list.add(SingleLineViewHolder(
+//                builder = ViewSingleLineBuilder.newBuilder()
+//                    .setStateIconLeft(
+//                        IconState.ShowFromRes(
+//                            iconId = item.theme.iconId,
+//                            colorState = ColorState.FromIntColor(item.theme.color)
+//                        )
+//                    )
+//                    .setStateTextLeft(
+//                        TextState.Show(
+//                            value = item.theme.name,
+//                            colorState = ColorState.FromIntColor(item.theme.color)
+//                        )
+//                    )
+//                    .setStateTextRight(
+//                        TextState.Show(
+//                            value = item.expense.amount.toString()
+//                        )
+//                    )
+//                    .setStateRoot(
+//                        RootState.Fill(
+//                            clickState = ClickState.Action {
+//                                toFullExpenseInfo(item)
+//                            }
+//                        )
+//                    )
+//                    .build()
+//            ))
+//        }
         _items.value = ListUiState.Success(list)
     }
 
